9e56f8cbebfcba7587f1209318757d36d7f62d05
Hafisa Albahri <hafisa.albahri@gmail.com>
2026-08-18 11:57:08 +0400

    Control Flow

--- Javascript Tasks/Task1/warmups.js
+++ Javascript Tasks/Task1/warmups.js
@@ -27,3 +27,39 @@ else {
     console.log("Username is empty");
 }
 
+// Control Flow
+
+const grades = [85, 92, 58, 73, 40];
+
+for (const grade of grades) {
+
+    if (grade >= 60) {
+        console.log(`${grade} -> Pass`);
+    }
+    else {
+        console.log(`${grade} -> Fail`);
+    }
+
+}
+
+
+const score = 85;
+
+switch (true) {
+
+    case score >= 90:
+        console.log("A");
+        break;
+
+    case score >= 80:
+        console.log("B");
+        break;
+
+    case score >= 70:
+        console.log("C");
+        break;
+
+    default:
+        console.log("F");
+
+}
