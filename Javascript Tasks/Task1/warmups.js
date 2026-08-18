aba1a1600815e37c9f9a36f2925258b6f5340d00
Hafisa Albahri <hafisa.albahri@gmail.com>
2026-08-18 11:39:29 +0400

    Variables & Dynamic Typing

---
+++ Javascript Tasks/Task1/warmups.js
@@ -0,0 +1,15 @@
+// Variables & Dynamic Typing
+
+const myName = "Hafisa";
+
+let value = 0;
+
+console.log(myName);
+
+console.log(typeof value);
+
+value = "hello";
+
+console.log(typeof value);
+
+<script src="warmups.js"></script>
