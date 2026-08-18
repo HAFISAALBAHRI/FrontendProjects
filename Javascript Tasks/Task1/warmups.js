15fe74b611065a88c65587ef10392d7acba29d19
Hafisa Albahri <hafisa.albahri@gmail.com>
2026-08-18 11:53:08 +0400

    Operators & Comparisons

--- Javascript Tasks/Task1/warmups.js
+++ Javascript Tasks/Task1/warmups.js
@@ -11,3 +11,19 @@ console.log(typeof value);
 value = "hello";
 
 console.log(typeof value);
+
+// Operators & Comparisons
+
+console.log("10" == 10);
+
+console.log("10" === 10);
+
+const userName = "";
+
+if (userName) {
+    console.log("Username exists");
+}
+else {
+    console.log("Username is empty");
+}
+
